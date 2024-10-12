ed32420fec074e71a749a2ce82badb341d46920b
Vega8991 <manuelvega011204@gmail.com>
2024-10-12 19:32:23 +0200

    Excercise 4 added

---
+++ Ejercicio4.java
@@ -0,0 +1,59 @@
+package Ejercicios;
+
+public class Ejercicio4 {
+
+	// Atributos
+
+	private String nombre;
+	private String color;
+	private int edad;
+	private int carrerasganadas;
+	private int carrerasperdidas;
+	private int trofeosganados;
+
+	// Getters y Setters
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public String getColor() {
+		return this.color;
+	}
+
+	public int getEdad() {
+		return this.edad;
+	}
+
+	public int getCarrerasganadas() {
+		return this.carrerasganadas;
+	}
+
+	public int getCarrerasperdidas() {
+		return this.carrerasperdidas;
+	}
+
+	public int getTrofeosganados() {
+		return this.trofeosganados;
+	}
+
+	public void setEdad(int newEdad) {
+		this.edad = newEdad;
+
+	}
+
+	public void setCarrerasganadas(int newCarrerasganadas) {
+		this.carrerasganadas = newCarrerasganadas;
+
+	}
+
+	public void setCarrerasperdidas(int newCarrerasperdidas) {
+		this.carrerasperdidas = newCarrerasperdidas;
+
+	}
+
+	public void setTrofeosganados(int newTrofeosganados) {
+		this.trofeosganados = newTrofeosganados;
+
+	}
+}
