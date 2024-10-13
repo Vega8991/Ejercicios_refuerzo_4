66c2dfa1a589eeb88dad00a0354cfe22712b148f
Manuel Vega Viñuelas <manuelvega011204@gmail.com>
2024-10-13 05:24:48 +0200

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,16 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        String nombre = "Manuel";
+        String apellidos = "Vega Vinuelas";
+        String direccion = "Cerro Blanco";
+        int telefono = 685583048;
+        String dni = "54436090N";
+
+            System.out.println("Hola buenas, soy " + nombre +" y mi apellido es " + apellidos);
+            System.out.println("Vivo en la calle " + direccion + " y mi telefono de contacto es el " + telefono);
+            System.out.println("Mi dni es el " + dni);
+
+
+    }
+}
